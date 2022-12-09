ee14a423741a8f3a3edb7d129a61b8496e68ecc0
unknown <duongquockhang.1205@gmail.com>
2022-12-09 11:20:32 +0200

    1 2 3 done

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ from 0 to 100
  */
 
 const printNum = () => {
-    for (var i = 0; i <= 100; i++) {
-        setTimeout(() => console.log(i), 1000)
-    }
-}
+  for (var i = 0; i <= 100; i++) {
+    setTimeout(console.log(i), 1000);
+  }
+};
 
-printNum()
+printNum();
 
 /*
 2. Given the array below:
@@ -22,25 +22,47 @@ You only need to produce the same array as expected result, no need to consider 
 possibility.
  */
 
-let myArr = ['12-24-2014', '09-2022-23', '12-30-2021', '08-02-2021', '07-15-2018', '2019-12-14', '2022-14-12']
+let myArr = [
+  "12-24-2014",
+  "09-2022-23",
+  "12-30-2021",
+  "08-02-2021",
+  "07-15-2018",
+  "2019-12-14",
+  "2022-14-12",
+];
 const fixDate = (array) => {
-    /* provide your code here */
-}
-let newArr = fixDate(myArr)
-console.log(newArr)
+  let fixedArray = [];
+
+  for (var i = 0; i < array.length; i++) {
+    const child = array[i].split("-");
+    child.sort(function (a, b) {
+      return a - b;
+    });
+    fixedArray.push(child[1] + "-" + child[0] + "-" + child[2]);
+  }
+  return fixedArray;
+};
+let newArr = fixDate(myArr);
+console.log(newArr);
 
 /*
 3. Counter function
 Write a counter funtion to print out in console the time difference between 2 given date
 Expected result in the console: 11 days - 13 hours - 38 minutes - 20 seconds
 */
-const dateFrom = new Date(500000)
-const dateTo = new Date(1000000000)
+const dateFrom = new Date(500000);
+const dateTo = new Date(1000000000);
 const counter = (from, to) => {
-    /* provide your code here */
-}
-const timer = counter()
-console.log(timer)
+  const day = dateTo.getDate() - dateFrom.getDate();
+  const hour = dateTo.getHours() - dateFrom.getHours();
+  const minute = dateTo.getMinutes() - dateFrom.getMinutes();
+  const second = dateTo.getSeconds() - dateFrom.getSeconds();
+
+  return `${day} days - ${hour} hours - ${minute} minutes - ${second} seconds`;
+};
+const timer = counter(dateFrom, dateTo);
+console.log(timer);
 
 /* 
 4. Check the url and read documentation: https://restcountries.com
@@ -50,14 +72,14 @@ The data fetched from url should be displayed in index.html.
 */
 
 const getAllCountries = () => {
-    /* provide your code here */
-}
+  /* provide your code here */
+};
 
 const getSingleCountry = () => {
-    /* provide your code here */
-}
+  /* provide your code here */
+};
 
-getAllCountries()
+getAllCountries();
 
 /*
 5. Provide logic for function generateNewFolderName, which receive an array as argument. Everytime the function gets called,
@@ -67,14 +89,14 @@ to array, and so on.
 */
 
 const generateNewFolderName = (existingFolders) => {
-    /*  provide your code here */
-}
+  /*  provide your code here */
+};
 
-let folder = []
-generateNewFolderName(folder)
-generateNewFolderName(folder)
-generateNewFolderName(folder)
-generateNewFolderName(folder)
+let folder = [];
+generateNewFolderName(folder);
+generateNewFolderName(folder);
+generateNewFolderName(folder);
+generateNewFolderName(folder);
 console.log(folder); //expect to see ['New Folder', 'New Folder (1)', 'New Folder (2)', 'New Folder (3)']
 
 /* 
@@ -91,14 +113,13 @@ Complete class TaxableBook:
 cost 14, profit 0.3 , tax 24% => expected price is 30.43
 */
 class Book {
-    _title
-    constructor(title, cost, profit) {
-    }
+  _title;
+  constructor(title, cost, profit) {}
 }
 
 class TaxableBook {
-    /* provide your code here */
+  /* provide your code here */
 }
 
-const book1 = new Book("The Power of Habits", 14, 0.3)
-const book2 = new TaxableBook("The Power of Habits", 14, 0.3, 24)
+const book1 = new Book("The Power of Habits", 14, 0.3);
+const book2 = new TaxableBook("The Power of Habits", 14, 0.3, 24);
